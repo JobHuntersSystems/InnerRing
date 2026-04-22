471445a76e00daa0cba39ef54207b9de0526504e
Martina Cañamares <martina.canamares@sarria.salesians.cat>
2026-04-22 17:09:00 +0200

    formularios creados

---
+++ PACS-Planet/PACS-Center/frmKeyManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PACS_Center
+{
+    public partial class frmKeyManager : Form
+    {
+        public frmKeyManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
