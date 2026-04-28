be9bdadc52fc6949faf97518624886a0eb6d6874
Damian Altamirano Ontiveros Rivero <daltamiranoontiveros@gmail.com>
2026-04-28 16:50:08 +0200

    feat: Interfaz inicial, controles personalizados y clases de UI
    Se han creado una maqueta de interfaz principal provisional, además de clases para la gestión de temas, así como proyectos para controles heredados y user controls

---
+++ PACS-Planet/PACS-Planet/frmMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PACS_UI;
+
+namespace PACS_Planet
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void AddLog(string message)
+        {
+            rtbLogs.AppendText($"[{DateTime.Now:HH:mm:ss}] [INFO ] {message}{Environment.NewLine}");
+            rtbLogs.SelectionStart = rtbLogs.Text.Length;
+            rtbLogs.ScrollToCaret();
+        }
+
+        private void btnSecuritySetup_Click(object sender, EventArgs e)
+        {
+            AddLog("asd");
+        }
+    }
+}
---
+++ PACS-Planet/PACS-UI/PacsTheme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace PACS_UI
+{
+    public static class PacsTheme
+    {
+        public static Color Background { get; } = Color.FromArgb(6, 10, 16);
+        public static Color Panel { get; } = Color.FromArgb(20, 25, 35);
+        public static Color NeonGreen { get; } = Color.Lime;
+        public static Color NeonCyan { get; } = Color.Cyan;
+        public static Color Warning { get; } = Color.Orange;
+        public static Color Danger { get; } = Color.Red;
+
+        public static Font TitleFont { get; } = new Font("Consolas", 14, FontStyle.Bold);
+        public static Font NormalFont { get; } = new Font("Consolas", 10, FontStyle.Regular);
+    }
+}
