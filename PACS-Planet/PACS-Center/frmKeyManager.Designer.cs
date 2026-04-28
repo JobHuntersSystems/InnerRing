576c41d0d887d24a4f8acc1dd6dc16e3f8a91d24
Martina Cañamares <martina.canamares@sarria.salesians.cat>
2026-04-28 15:35:32 +0200

    keyGenerator creado

--- PACS-Planet/PACS-Center/frmKeyManager.Designer.cs
+++ PACS-Planet/PACS-Center/frmKeyManager.Designer.cs
@@ -29,19 +29,44 @@ namespace PACS_Center
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnKeyGenerator = new System.Windows.Forms.Button();
+            this.tbx_container = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnKeyGenerator
+            // 
+            this.btnKeyGenerator.Location = new System.Drawing.Point(114, 101);
+            this.btnKeyGenerator.Name = "btnKeyGenerator";
+            this.btnKeyGenerator.Size = new System.Drawing.Size(303, 102);
+            this.btnKeyGenerator.TabIndex = 0;
+            this.btnKeyGenerator.Text = "Key Generator";
+            this.btnKeyGenerator.UseVisualStyleBackColor = true;
+            this.btnKeyGenerator.Click += new System.EventHandler(this.btnKeyGenerator_Click);
+            // 
+            // tbx_container
+            // 
+            this.tbx_container.Location = new System.Drawing.Point(114, 261);
+            this.tbx_container.Name = "tbx_container";
+            this.tbx_container.Size = new System.Drawing.Size(318, 26);
+            this.tbx_container.TabIndex = 1;
             // 
             // frmKeyManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1106, 624);
+            this.Controls.Add(this.tbx_container);
+            this.Controls.Add(this.btnKeyGenerator);
             this.Name = "frmKeyManager";
             this.Text = "frmKeyManager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnKeyGenerator;
+        private System.Windows.Forms.TextBox tbx_container;
     }
 }
--- PACS-Planet/PACS-Center/frmKeyManager.cs
+++ PACS-Planet/PACS-Center/frmKeyManager.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace PACS_Center
 {
@@ -15,6 +17,27 @@ namespace PACS_Center
         public frmKeyManager()
         {
             InitializeComponent();
+        }
+
+        private void btnKeyGenerator_Click(object sender, EventArgs e)
+        {
+            string keyName = tbx_container.Text;
+            if (string.IsNullOrEmpty(keyName))
+            {
+                MessageBox.Show("Rellene el nombre de la key para poder seguir con el proceso");
+                return;
+            }
+
+            CspParameters cspp = new CspParameters { KeyContainerName = keyName};
+
+            using(RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspp))
+            {
+                rsa.PersistKeyInCsp = true;
+                string publicKey = rsa.ToXmlString(false);
+
+                string filePath = $"{keyName}_PublicKey.xml";
+                File.WriteAllText(filePath, publicKey);
+            }
         }
     }
 }
