902cbb1f8f4b1fd4f1c2f6ceadd0d29d9d84b72b
Martina Cañamares <martina.canamares@sarria.salesians.cat>
2026-05-13 07:29:59 +0200

    fix PACS Center

---
+++ PACS-Planet/PACS-Center/KeyManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inner_DB_Access;
+using System.Data;
+using System.Security.Cryptography;
+
+namespace PACS_Center
+{
+    public class clsKeyManager
+    {
+        private DB_CRUD db = new DB_CRUD();
+        public string SearchCode(int idPlanet)
+        {
+            string code = "";
+            
+            string queryCode = $"SELECT CodePlanet FROM Planets WHERE idPlanet = {idPlanet}";
+            DataTable dt = db.PortarDataTable(queryCode);
+
+            if (dt.Rows.Count > 0)
+            {
+                code = dt.Rows[0]["CodePlanet"].ToString();
+            }
+            return code;
+        }
+
+        //----------------Guardar en la base de datos los datos--------------------------------\\
+        public void SaveDataBase(string key, int idPlanet)
+        {
+            string queryCheck = $"SELECT idPlanet FROM PlanetKeys WHERE idPlanet = {idPlanet}";
+            DataTable dt = db.PortarDataTable(queryCheck);
+
+            string query;
+            if (dt.Rows.Count > 0)
+            {
+                query = $"UPDATE PlanetKeys SET XMLKey = '{key}' WHERE idPlanet = {idPlanet}";
+            }
+            else
+            {
+                query = $"INSERT INTO PlanetKeys (idPlanet, XMLKey) VALUES ({idPlanet}, '{key}')";
+            }
+            db.Executa(query);
+        }
+
+        //-------------------------Generar keys----------------------\\
+        public string GenerateRsaKey(string containerName)
+        {
+            CspParameters cspp = new CspParameters { KeyContainerName = containerName };
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspp))
+            {
+                rsa.PersistKeyInCsp = true;
+                return rsa.ToXmlString(false);
+            }
+        }
+    }
+}
--- PACS-Planet/PACS-Center/frmInnerEncryption.cs
+++ PACS-Planet/PACS-Center/frmInnerEncryption.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Inner_DB_Access;
 using PACS_Common;
 
 namespace PACS_Center
@@ -23,100 +20,25 @@ namespace PACS_Center
 
         private Dictionary<char, string> data;
         private string validationCode;
-        Random rng = new Random();
         private int _idPlanet;
         int pass;
-
-        //----------------Genera el codigo de 12 caracteres y lo guarda en la bbdd ------------------------------\\
-        private string RandomCode()
-        {
-            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-            byte[] data = new byte[12];
-            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
-            {
-                crypto.GetBytes(data);
-            }
-            StringBuilder result = new StringBuilder(12);
-            
-            foreach (byte b in data)
-            {
-                int num = b % (chars.Length);
-                result.Append(chars[num]);
-            }
-            return result.ToString();
-        }
-
-        //----------------Genera el diccionario de cifrado letra ------------------------------\\
-        private Dictionary<char, string> RandomValue()
-        {
-            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-            Dictionary<char, string> keyValuePairs = new Dictionary<char, string>();
-            HashSet<string> used = new HashSet<string>();
-
-            foreach (var let in letters)
-            {
-                string value;
-                do
-                {
-                    value = RandomNumber();
-                } while (used.Contains(value));
-                used.Add(value);
-
-                keyValuePairs.Add(let, value);
-            }
-            return keyValuePairs;
-        }
-        private string RandomNumber()
-        {
-            int num = rng.Next(0, 1000);
-            return num.ToString().PadLeft(3, '0');
-        }
-        
+        InnerEncryption cls = new InnerEncryption();
+ 
         //----------------Guardar en la base de datos los datos--------------------------------\\
-        private void SaveCode()
+        private void Code()
         {
             try
             {
-                DB_CRUD db = new DB_CRUD();
-                string id = $"SELECT idInnerEncryption FROM InnerEncryption WHERE idPlanet = {_idPlanet}";
-                DataTable dt = db.PortarDataTable(id);
-
-                if(dt.Rows.Count == 0)
-                {
-                    string msj = "The planet wasn't in the database, now I register it. One moment...";
-                    lstMsj.AddLog(LogLevel.Info, msj);
-                    string queryInsert = $"INSERT INTO InnerEncryption (idPlanet, ValidationCode) " +
-                                         $"VALUES ({_idPlanet}, '{validationCode}')";
-                    db.Executa(queryInsert);
-                    dt = db.PortarDataTable(id);
-                }
-                int idInner = (int)dt.Rows[0]["idInnerEncryption"];
-
-                string queryCode = $"Update InnerEncryption SET ValidationCode = '{validationCode}' WHERE idPlanet = {_idPlanet}";
-                db.Executa(queryCode);
-
-                string queryDelete = $"DELETE FROM InnerEncryptionData WHERE idInnerEncryption = {idInner}";
-                db.Executa(queryDelete);
-
-                foreach (KeyValuePair<char, string> item in data)
-                {
-                    string queryInsert = $"INSERT INTO InnerEncryptionData (idInnerEncryption, Word, Numbers) " +
-                                         $"VALUES ({idInner}, '{item.Key}', '{item.Value}')";
-
-                    db.Executa(queryInsert);
-                }
+                cls.SaveCode(_idPlanet, validationCode, data);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 timerMsj.Stop();
                 btnCode.Enabled = true;
-                string msj = "Error uploading to database!";
-                lstMsj.AddLog(LogLevel.Error, msj);
-
+                lstMsj.AddLog(LogLevel.Error, "Error uploading to database!");
                 MessageBox.Show(ex.Message);
             }
         }
-
         private void btnCode_Click(object sender, EventArgs e)
         {
             btnCode.Enabled = false;
@@ -135,8 +57,8 @@ namespace PACS_Center
                 case 0:
                     string msj = "Generating encoding...";
                     lstMsj.AddLog(LogLevel.Info, msj);
-                    validationCode = RandomCode();
-                    data = RandomValue();
+                    validationCode = cls.RandomCode();
+                    data = cls.RandomValue();
                     break;
                 case 1:
                     msj = "Validating encoding...";
@@ -153,7 +75,7 @@ namespace PACS_Center
                     timerMsj.Interval = 1500;
                     break;
                 case 4:
-                    SaveCode();
+                    cls.SaveCode();
                     timerMsj.Interval = 2000;
                     break;
                 case 5:
