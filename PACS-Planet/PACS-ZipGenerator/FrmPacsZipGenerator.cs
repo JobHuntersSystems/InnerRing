9c318cd41c7d64e9afd3f4fb882aeb178bface45
Axololt9597 <axel.fajardo@sarria.salesians.cat>
2026-05-14 18:04:08 +0200

    feat: components software planets axel (#25)
    * feature: formulari_Comunicacio-Axel
    
    * feat: Add basic Planet TCP/IP startup
    
    * Update: MainForm
    
    * feature: FrmPlanetValidation
    
    * Update: FrmPlanetValidation
    
    * Fix: Simulation succesfull
    
    * Removed: My FrmMain
    
    * Update: FrmPlanetValidation
    
    * feature: frmZip
    
    * Update:  FilesAndZIPGenerations
    
    * Update: ChecksumGenerated
    
    * Update: minimal changes
    
    * Update: Creation of the Forms
    
    * Update: ClassLibrary
    
    * Update: Finished Class Library
    
    * Fix: Pull Request
    
    * Fix: Pull Request

---
+++ PACS-Planet/PACS-ZipGenerator/FrmPacsZipGenerator.cs
@@ -0,0 +1,85 @@
+﻿using PACS_Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PACS_Services;
+
+namespace PACS_ZipGenerator
+{
+	public partial class FrmPacsZipGenerator : Form
+	{
+
+		private PacsZipService zipService;
+		private PacsZipService.PacsZipResult lastZipResult;
+
+		public FrmPacsZipGenerator()
+		{
+			InitializeComponent();
+
+			InitializeZipGenerator();
+		}
+
+		private void InitializeZipGenerator()
+		{
+			zipService = new PacsZipService();
+
+			// Nos suscribimos al evento para recibir aviso cuando el ZIP esté listo.
+			zipService.ZipGenerated += ZipService_ZipGenerated;
+
+			lstGenerator.AddLog(LogLevel.Info, "PACS ZIP generator loaded.");
+			lstGenerator.AddLog(LogLevel.Info, "Waiting for operator command.");
+		}
+
+		private void btnGenerator_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				btnGenerator.Enabled = false;
+
+				lstGenerator.AddLog(LogLevel.Info, "Generating PACS files...");
+				lstGenerator.AddLog(LogLevel.Info, "Creating PACS.zip...");
+
+				// Application.StartupPath apunta a la carpeta donde se ejecuta la aplicación.
+				// Ahí debe estar PACS_Config.xml.
+				lastZipResult = zipService.GeneratePacsZip(Application.StartupPath);
+
+				lstGenerator.AddLog(LogLevel.Success, "ZIP generation process completed.");
+			}
+			catch (Exception ex)
+			{
+				lstGenerator.AddLog(LogLevel.Error, "ZIP generation failed.");
+
+				MessageBox.Show(
+					ex.Message,
+					"PACS ZIP error",
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Error
+				);
+			}
+			finally
+			{
+				btnGenerator.Enabled = true;
+			}
+		}
+
+		private void ZipService_ZipGenerated(object sender, EventArgs e)
+		{
+			PacsZipService.ZipGeneratedEventArgs args =
+				(PacsZipService.ZipGeneratedEventArgs)e;
+
+			lastZipResult = args.Result;
+
+			lstGenerator.AddLog(LogLevel.Success, "PACS.zip created successfully.");
+			lstGenerator.AddLog(LogLevel.Info, "Generated files: " + args.Result.GeneratedFiles);
+			lstGenerator.AddLog(LogLevel.Info, "Letters per file: " + args.Result.LettersPerFile);
+			lstGenerator.AddLog(LogLevel.Info, "ZIP size: " + args.Result.ZipSizeBytes + " bytes");
+			lstGenerator.AddLog(LogLevel.Info, "ZIP path: " + args.Result.ZipPath);
+		}
+	}
+}
